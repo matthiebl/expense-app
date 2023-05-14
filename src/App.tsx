820290761a0fdf41c9705a09c5209f7513b48183
Matthew Hiebl <87024986+matthiebl@users.noreply.github.com>
2023-05-14 11:25:01 +1000

    Add start of rework

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Router } from './resources/routes'
 
 const App = () => (
     <BrowserRouter>
         <Routes>
-            <Route path='/' element={<b>Hi</b>} />
+            <Route path={Router.home.path} element={<Dashboard />} />
+            <Route path={Router.summary.path} element={<Summary />} />
+            <Route path={Router.income.path} element={<Income />} />
+            <Route path={Router.expenses.path} element={<Expenses />} />
+            <Route path={Router.invest.path} element={<Investments />} />
+            <Route path={Router.add.path} element={<NewEntry />} />
         </Routes>
     </BrowserRouter>
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: { 500: '#4BBEDA', 900: '#2A83A1' },
+                secondary: { 500: '#9B6FED', 900: '#974CEE' },
+                alt: { 500: '#F58878', 900: '#C35F53' },
+                alt2: { 500: '#F59161', 900: '#EA855F' },
+                back: {
+                    500: '#4B4D6A',
+                    600: '#333248',
+                    700: '#2F3049',
+                    900: '#181B2B',
+                },
+            },
+            screens: {
+                '3xl': '1750px',
+                '4xl': '2000px',
+                '5xl': '2250px',
+                '6xl': '2500px',
+            },
+        },
     },
     plugins: [require('@tailwindcss/forms')],
 }
